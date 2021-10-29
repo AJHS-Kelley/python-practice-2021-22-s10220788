7c703e4a1c5651a2888c5d972b1c0985effd394c
Kayln J <s10220788@students.duvalschools.org>
2021-10-29 09:21:53 -0400

    [5B] v0.1
    Added integer and floating point variable.

--- variables/variables_practice.py
+++ variables/variables_practice.py
@@ -1,1 +1,8 @@
-# Variables Practice, Johnson Kayln, 10/29/21 9:10 am version 0.0
+# Variables Practice, Johnson Kayln, 10/29/21 9:10am, version 0.1
+
+# Three Main Types of Variables for this Class
+
+health = 125 # INTEGERS, any +/- whole number AND 0.
+account_balance = 16355.92 # FLOATING POINTS, any +/- with decimal.
+
+
