9465ef00b9da27e54c29e3a08c4782abd323d4eb
Kayln J <s10220788@students.duvalschools.org>
2021-10-29 09:33:04 -0400

    [5B] v0.1
    Added print statements for variables.

--- variables/variables_practice.py
+++ variables/variables_practice.py
@@ -4,5 +4,8 @@
 
 health = 125 # INTEGERS, any +/- whole number AND 0.
 account_balance = 16355.92 # FLOATING POINTS, any +/- with decimal.
+user_name = "Dunnbel Jefferson" # STRING, letters/numbers printed on screen.
 
-
+print(health) 
+print(account_balance)
+print(user_name)
